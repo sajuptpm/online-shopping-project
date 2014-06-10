b596b663b0f0bf165298e57cebee5ce7618ed667
Saju Madhavan <sajuptpm@gmail.com>
2014-06-10 14:21:35 +0530

    10/06/2014

--- book/models.py
+++ book/models.py
@@ -1,39 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
 
-class Usertb(models.Model):
-	uname = models.CharField(max_length=200)
-	password= models.CharField(max_length=50)
-	emailid=models.EmailField(max_length=100)
-	mobile=models.CharField(max_length=10)
+#    user = models.OneToOneField(User)
+   # address = models.CharField(max_length=100)
+   # pincode = models.IntegerField()
+   # mobile = models.CharField(max_length=10)
 
-class Catagorytb(models.Model):
+
+
+class Catagory(models.Model):
 	cname=models.CharField(max_length=200)
 	purdate=models.DateTimeField('purchase-date')
-	createdby=models.ForeignKey(Usertb)
+	createdby=models.ForeignKey(User)
 
-class Producttb(models.Model):
+class Product(models.Model):
+	
 	pname=models.CharField(max_length=200)
 	discription=models.CharField(max_length=500)
-	#price=models.IntegerField()
-	#noitem=models.IntegerField()
-	createdby=models.ForeignKey(Usertb)
-	cid=models.ForeignKey(Catagorytb)
-
-class Carttb(models.Model):
-	pid=models.ForeignKey(Producttb)
+	price=models.IntegerField()
 	noitem=models.IntegerField()
-	totalprice=models.IntegerField()
-	status=models.CharField(max_length=10)
+	createdby=models.ForeignKey(User)
+	cid=models.ForeignKey(Catagory)
+	photo=models.ImageField(upload_to='photo') #image in pjct/media/photo
+class Cart(models.Model):
+	uid=models.ForeignKey(User)
+	pid=models.ForeignKey(Product)
+	noitem=models.IntegerField()
 	purdate=models.DateTimeField("purchase-date")
 	deldate=models.DateTimeField("delivary-date")
-
-
-
-
-		
-
-	
-		
-		
