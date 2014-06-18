3d07d6791afcbbff943b6b70f2945e9a5400ddc9
Saju Madhavan <sajuptpm@gmail.com>
2014-06-18 12:47:40 +0530

    update email settings

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -36,10 +36,11 @@ ALLOWED_HOSTS = []
 EMAIL_HOST      = 'localhost'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST_USER = ''
-EMAIL_PORT      = 1025
+EMAIL_PORT      = 25
 EMAIL_USE_TLS   = False
 DEFAULT_FROM_EMAIL = 'webmaster@localhost'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
